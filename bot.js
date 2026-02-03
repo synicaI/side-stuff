2c80dd50c62f0ba295633f1d125274b116ae4d46
synicaI <okbut20@gmail.com>
2026-02-03 15:40:35 +0100

    Update bot.js and move bot.js to bot

--- bot.js
+++ bot.js
@@ -3,4 +3,4 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => console.log('Discord Bot Online')); // <- optional: rename message
 
-client.login(process.env.BOT_TOKEN); // Discord token from Railway environment variable
+client.login(process.env.BOT_TOKEN); // Discord token from Railway environment variable a
