4e0df74898130b061e067008fa841c855080c140
synicaI <okbut20@gmail.com>
2026-02-03 15:41:31 +0100

    Create bot.js

---
+++ bot/bot.js
@@ -0,0 +1,5 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => console.log('Discord Bot Online'));
+client.login(process.env.BOT_TOKEN);
