1ce4b366d8036dab270e435267bed43cfab734bc
synicaI <okbut20@gmail.com>
2026-02-03 15:23:37 +0100

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,1 +1,28 @@
-const express = require('express'); const router = express.Router(); router.get('/', (req,res)= route placeholder')); module.exports = router; 
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const { SECRET } = require('../config');
+
+router.get('/', (req, res) => {
+    const { key, hwid, secret } = req.query;
+
+    if (secret !== SECRET) return res.status(403).send("Invalid secret");
+
+    db.get("SELECT * FROM users WHERE key = ?", [key], (err, user) => {
+        if (err) return res.status(500).send("Server error");
+        if (!user) return res.status(401).send("Invalid key");
+        if (user.banned) return res.status(403).send("Banned");
+        if (Date.now() > user.expires) return res.status(403).send("Expired");
+
+        // Lock HWID
+        if (!user.hwid) {
+            db.run("UPDATE users SET hwid = ? WHERE key = ?", [hwid, key]);
+        } else if (user.hwid !== hwid) {
+            return res.status(403).send("HWID mismatch");
+        }
+
+        res.status(200).send("OK");
+    });
+});
+
+module.exports = router;
