fafaa01947a147299f21b90541424d3813119d70
synicaI <okbut20@gmail.com>
2026-02-03 15:23:15 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,13 @@
-const sqlite3 = require('sqlite3').verbose(); module.exports = new sqlite3.Database('./database.db'); 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.db');
+
+// Create users table if it doesn't exist
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    key TEXT UNIQUE,
+    hwid TEXT,
+    expires INTEGER,
+    banned INTEGER DEFAULT 0
+)`);
+
+module.exports = db;
